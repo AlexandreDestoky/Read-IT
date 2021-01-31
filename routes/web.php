cd885326637787c76efb0c076461242ebc81fa35
Destoky <adestoky@gmail.com>
2021-01-31 21:24:43 +0100

    route posts.show
    title et content

--- routes/web.php
+++ routes/web.php
@@ -29,3 +29,13 @@ Route::get('/', [Posts::class, 'index'])->name('blog');
 // CTRL: Posts
 // ACTION: index
 Route::get('/posts', [Posts::class, 'index'])->name('posts.index');
+
+
+// DETAILS D'UN POST
+// PATTERN: /posts/post/slug
+// CTRL: Posts
+// ACTION: show
+Route::get('/posts/{post}/{slug}', [Posts::class, 'show'])
+  ->where('post', '[1-9][0-9]*')
+  ->where('slug', '[a-z0-9][a-z0-9\-]*')
+  ->name('posts.show');
